159e964c95448b369b84faff0b562f67eca5fbae
wilsonge <georgejameswilson@googlemail.com>
2019-11-22 12:04:17 +0000

    Allow saving the publishing to the transition table. Remove PHP Errors. General Cleanup

--- plugins/workflow/publishing/publishing.php
+++ plugins/workflow/publishing/publishing.php
@@ -15,7 +15,7 @@ use Joomla\CMS\Form\Form;
 /**
  * Publishing handling for workflow items
  *
- * @since  4.0.0
+ * @since  __DEPLOY_VERSION__
  */
 class PlgWorkflowPublishing extends CMSPlugin
 {
@@ -23,21 +23,41 @@ class PlgWorkflowPublishing extends CMSPlugin
 	 * Load the language file on instantiation.
 	 *
 	 * @var    boolean
-	 * @since  4.0.0
+	 * @since  __DEPLOY_VERSION__
 	 */
 	protected $autoloadLanguage = true;
 
 	/**
 	 * Add additional fields to the supported forms
 	 *
-	 * @param   Form  $form  The form to be altered.
+	 * @param   Form   $form  The form to be altered.
 	 * @param   mixed  $data  The associated data for the form.
 	 *
 	 * @return  boolean
 	 *
-	 * @since   4.0.0
+	 * @since   __DEPLOY_VERSION__
 	 */
 	public function onContentPrepareForm(Form $form, $data)
+	{
+		// Check we are manipulating the workflow form.
+		if ($form->getName() === 'com_workflow.transition')
+		{
+			$this->loadPublishingFormForTransitionView($form);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Adds the publishing field to the transition view
+	 *
+	 * @param   Form   $form  The form to be altered.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function loadPublishingFormForTransitionView($form)
 	{
 		Form::addFormPath(__DIR__ . '/forms');
 
