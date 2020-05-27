d23f5c12ef007f24f435c8845fbb4c8f20cd180e
Brian Teeman <brian@teeman.net>
2020-05-27 08:47:29 +0100

    Update administrator/components/com_workflow/tmpl/workflows/default.php
    Co-authored-by: Benjamin Trenkle <bembelimen@users.noreply.github.com>

--- administrator/components/com_workflow/tmpl/workflows/default.php
+++ administrator/components/com_workflow/tmpl/workflows/default.php
@@ -75,7 +75,7 @@ $userId = $user->id;
 								<th scope="col" class="w-1 text-center d-none d-md-table-cell">
 									<?php echo HTMLHelper::_('searchtools.sort', '', 'w.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
 								</th>
-								<th scope="col" class="w-1 text-center d-md-table-cell">
+								<th scope="col" class="w-1 text-center">
 									<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'w.published', $listDirn, $listOrder); ?>
 								</th>
 								<th>
