05eb4df0f284b1d176e8041ebd74c367b232de6c
Brian Teeman <brian@teeman.net>
2020-05-27 08:47:36 +0100

    Update administrator/components/com_workflow/tmpl/transitions/default.php
    Co-authored-by: Benjamin Trenkle <bembelimen@users.noreply.github.com>

--- administrator/components/com_workflow/tmpl/transitions/default.php
+++ administrator/components/com_workflow/tmpl/transitions/default.php
@@ -75,7 +75,7 @@ if ($saveOrder)
 								<th scope="col" class="w-20">
 									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_FROM_STAGE', 'from_stage', $listDirn, $listOrder); ?>
 								</th>
-								<th scope="col" class="w-20 d-md-table-cell">
+								<th scope="col" class="w-20">
 									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_TO_STAGE', 'to_stage', $listDirn, $listOrder); ?>
 								</th>
 								<th scope="col" class="w-3 d-none d-md-table-cell">
